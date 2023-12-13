6d3a15742f0ec95c21a357b30754c7ddd70f14ef
James <james.ekasiba@student.moringaschool.com>
2023-12-13 21:55:47 +0300

    Completed Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def is_anagram(self, candidate):
+        return sorted(self.word) == sorted(candidate.lower()) and self.word != candidate.lower()
+
+    def match(self, word_list):
+        return [candidate for candidate in word_list if self.is_anagram(candidate)]
+
+
+listen = Anagram("listen")
+result = listen.match(['enlists', 'google', 'inlets', 'banana'])
+print(result)
